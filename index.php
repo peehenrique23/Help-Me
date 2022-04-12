3e83e3b4cf6b97a4796b9d93003790f1d5ff4e6f
peehenrique23 <98557314+peehenrique23@users.noreply.github.com>
2022-04-11 22:31:57 -0300

    Commit Title

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style-main.css">
-    <title>HM - Help Me</title>
+    <title>Help Me</title>
 </head>
 <body>
     <h1 id="t_central">HELP ME</h1>
